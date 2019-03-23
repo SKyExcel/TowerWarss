cca6180b8784877eb7f61894371c8425e7bb50d7
nutyworks <nutyworks@gmail.com>
2019-03-23 19:33:06 +0900

    Create ActionBar.java

---
+++ src/main/java/towerwar/util/ActionBar.java
@@ -0,0 +1,38 @@
+package towerwar.util;
+
+import net.minecraft.server.v1_12_R1.ChatMessageType;
+import net.minecraft.server.v1_12_R1.IChatBaseComponent;
+import net.minecraft.server.v1_12_R1.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class ActionBar {
+
+    private String message;
+
+    public ActionBar(String message) {
+        this.message = message;
+    }
+
+    public void send(Player player) {
+        PacketPlayOutChat packetPlayOutChat = new PacketPlayOutChat(
+                IChatBaseComponent.ChatSerializer.a(message),
+                ChatMessageType.GAME_INFO);
+
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetPlayOutChat);
+    }
+
+    public void send(Player... players) {
+        for (Player player : players) {
+            send(player);
+        }
+    }
+
+    public void send(List<Player> players) {
+        for (Player player : players) {
+            send(player);
+        }
+    }
+}
